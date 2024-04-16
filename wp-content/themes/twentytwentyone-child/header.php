f854987e221df62f973bb854261fee43992c832d
Roselyn <medrano727@gmail.com>
2024-04-16 10:53:09 -0400

    final commit

--- wp-content/themes/twentytwentyone-child/header.php
+++ wp-content/themes/twentytwentyone-child/header.php
@@ -24,7 +24,7 @@
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','UPDATE_ME_W_GTM_ID');</script>
+	})(window,document,'script','dataLayer','GTM-P2PBT8ZR');</script>
 	<!-- End Google Tag Manager -->
 	<?php wp_head(); ?>
 </head>
@@ -43,17 +43,78 @@
 					<!-- Main jumbotron for a primary marketing message or call to action -->
 					<div class="default-max-width">
 						<div>
-							<h1>Hello!</h1>
-							<p>Welcome to our engineering practical. This intro is hard-coded into our child template and our Marketing team would like you to update the template so this intro can be edited from the Wordpress Admin.<br /><br /></p>
-							<p>Please also add a little information to your page about your favorite hobby, we'd love to get to know you better.<br /><br /></p>
-							
-							<p>We also added some Analytics requirements to the page but didn't implement them. They should be easy to find, good luck!<br /><br /></p>
-							<p></p>
-							<p><button id="testButton" class="btn btn-success" style="display:none;" onclick="loadPopup();" title="Bind an event to me!">Subscription Popup!</button></p>
+							<p><button id="testButton" class="btn btn-success"  onclick="loadPopup();" title="Bind an event to me!">Subscription Popup!</button></p>
 						</div>
 					</div>
 
 					<div id="mainContent" class="container">
 
 					</div> <!-- /container -->
-					<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> -->
+					<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>
+	function hideModal(){
+	var modal = document.getElementById("testModal");
+  //removed to show button - roselyn
+	// modal.style.display = "none";
+}
+
+function sayHello(){
+	window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-5Z1DJ5DEQQ');
+	var hello = document.getElementById("say").value;
+	alert(hello);
+  gtag('event', "EventTwo", {
+    'event_category': 'Say Hello',
+    'event_label': hello,
+    'event_action': "Say It!",
+  });
+}
+
+
+function loadPopup() {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-5Z1DJ5DEQQ');
+
+  buttonText = document.getElementById("testButton").innerHTML;
+  gtag('event', "EventOne", {
+    'event_category': 'Button Interaction',
+    'event_label': buttonText,
+    'event_action': "Click",
+  });
+	var popUp = `
+	<div class="default-max-width">
+        <div id="testModal" class="modal" tabindex="-1" role="dialog" style="display:block">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" onclick="hideModal();" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Test Popup!</h4>
+              </div>
+              <div class="modal-body">
+                <p>
+                  <form>
+                    <div class="form-group">
+                      <label for="say">Say Hello:</label>
+                      <input type="text" id="say" class="form-control" placeholder="Hello Wheelhouse!" name="say">
+                    </div>
+                  </form>
+                </p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" onclick="hideModal();" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="submit" onclick="sayHello();" class="btn btn-primary">Say It!</button>
+              </div>
+            </div><!-- /.modal-content -->
+          </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+      </div>
+      `;
+
+      var content = document.getElementById("mainContent");
+      content.innerHTML = popUp;
+}
+</script>
